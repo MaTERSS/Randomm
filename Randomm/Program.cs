22358cb20a1d16c93f2601b677583e75c16627d8
Ardor <Ardor@DESKTOP-BQF7240>
2024-11-11 16:25:47 +0800

    Добавьте файлы проекта.

---
+++ Randomm/Program.cs
@@ -0,0 +1,29 @@
+﻿/*С помощью Random получить число number, которое не больше 100. 
+  Найти сумму всех положительных чисел меньше number (включая число), которые кратные 3 или 5. 
+  (К примеру, это числа 3, 5, 6, 9, 10, 12, 15 и т.д.)*/
+using System;
+using System.Text;
+
+namespace CSharplight
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 100);
+            Console.WriteLine($"Сгенерированное число: {number}");
+
+            int sum = 0;
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum = i++;
+                }
+            }
+            Console.WriteLine($"Сумма всех положительных чисел, кратных 3 или 5, меньше {number}: {sum}");
+        }
+    }
+}
